7989703ed8de10927f6111ed1b252275b28748cd
unknown <frarsa18@gmail.com>
2024-03-25 07:19:39 +0700

    commit kedua

---
+++ modul6_1302223137/SayaTubeUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+public class SayaTubeUser
+{
+	private int id;
+	private List<SayaTubeVideo> uploadedVideos;
+	private string usn;
+	public SayaTubeUser(string Username)
+    {
+        Debug.Assert(Username != null);
+        Debug.Assert(Username.Length < 101);
+        usn = Username;
+        uploadedVideos = new List<SayaTubeVideo>();
+
+    }
+
+	public int GetTotalVideoPlayCount()
+    {
+        int count = 0;
+        foreach()
+        return count;
+    }
+
+    public void AddVideo(SayaTubeVideo uploadedVideos)
+    {
+        
+    }
+
+    public void PrintAllVideoPlayCount()
+    {
+        Console.WriteLine("User: " + Username);
+        Console.WriteLine("Video 1 judul: " + title)
+    }
+}
